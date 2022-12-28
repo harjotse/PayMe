e8cf4689889381a8e69a5826a9074f692ed6e9ae
unknown <bhatiaharjot2000@gmail.com>
2022-12-28 23:22:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/payme/QRScanner.java
@@ -0,0 +1,59 @@
+package com.example.payme;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+public class QRScanner extends AppCompatActivity {
+    com.budiyev.android.codescanner.CodeScanner codeScanner;
+   // CodeScanner mcodeScanner;
+    CodeScannerView scannView;
+    TextView resultData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_scanner);
+        scannView= findViewById(R.id.scannerView);
+        codeScanner = new com.budiyev.android.codescanner.CodeScanner(this,scannView);
+        resultData=findViewById(R.id.resultOfQR);
+
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(QRScanner.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                codeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        codeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        codeScanner.releaseResources();
+        super.onPause();
+    }
+}
